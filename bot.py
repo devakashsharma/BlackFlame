f2441db910425548b285115ef50e47c430b6f93c
Akash Sharma <soulakash20002@gmail.com>
2025-04-24 13:42:25 +0530

    update

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Replace with your token
-BOT_TOKEN = "7810013958:AAExxBHqH3No9CTMWE9zrTNbp714cHRQAso"
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+bot_token = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I am your AI summarizer bot 🤖")
@@ -11,7 +15,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user.first_name
     await update.message.reply_text(f"Hello {user}!")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token(bot_token).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("hello", hello))
